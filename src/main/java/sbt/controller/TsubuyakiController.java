eba8870e4f607c86cba74c11f21bc388d7a8935e
mamorum <mamorum@users.noreply.github.com>
2024-03-12 20:13:47 +0900

    Update spring api.

--- src/main/java/sbt/controller/TsubuyakiController.java
+++ src/main/java/sbt/controller/TsubuyakiController.java
@@ -3,51 +3,54 @@ package sbt.controller;
 import java.util.Collections;
 import java.util.Map;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import sbt.model.Tsubuyaki;
 import sbt.repository.TsubuyakiRepository;
 
-@RestController @RequestMapping("/tsubuyaki")
+@RestController
+@RequestMapping("/tsubuyaki")
 public class TsubuyakiController {
 
-	@Autowired TsubuyakiRepository repo;
+  @Autowired
+  TsubuyakiRepository repo;
 
-	@RequestMapping(method=RequestMethod.POST)
-	public Map<String, Tsubuyaki> create(
-			@Valid @RequestBody Tsubuyaki tsubuyaki
-	) {
-		return Collections.singletonMap(
-			"tsubuyaki", repo.save(tsubuyaki)
-		);
-	}
+  @PostMapping
+  public Map<String, Tsubuyaki> create(
+    @RequestBody Tsubuyaki tsubuyaki
+  ) {
+    return Collections.singletonMap(
+      "tsubuyaki", repo.save(tsubuyaki)
+    );
+  }
 
-	@RequestMapping(method=RequestMethod.GET)
-	public Map<String, Iterable<Tsubuyaki>> read() {
-		return Collections.singletonMap(
-			"tsubuyaki", repo.findAllByOrderByUpdatedTimeDesc()
-		);
-	}
+  @GetMapping
+  public Map<String, Iterable<Tsubuyaki>> read() {
+    return Collections.singletonMap(
+      "tsubuyaki", repo.findAllByOrderByUpdatedTimeDesc()
+    );
+  }
 
-	@RequestMapping(path="/{id}", method=RequestMethod.PUT)
-	public void update(
-			@PathVariable Long id, @RequestParam String txt
-	) {
-		Tsubuyaki tsubuyaki = repo.findOne(id);
-		tsubuyaki.txt = txt;
-		repo.save(tsubuyaki);
-	}
+  @PutMapping("/{id}")
+  public void update(
+    @PathVariable Long id, @RequestParam String txt
+  ) {
+    Tsubuyaki tsubuyaki = repo.findById(id).get();
+    tsubuyaki.txt = txt;
+    repo.save(tsubuyaki);
+  }
 
-	@RequestMapping(path="/{id}", method=RequestMethod.DELETE)
-	public void delete(@PathVariable Long id) {
-		repo.delete(id);
-	}
+  @DeleteMapping("/{id}")
+  public void delete(@PathVariable Long id) {
+    repo.deleteById(id);
+  }
 }
