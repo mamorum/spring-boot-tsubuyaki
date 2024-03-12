f82b16575d99213eedb7372a9426ad2d59df8f9d
mamorum <mamorum@users.noreply.github.com>
2024-03-12 20:30:44 +0900

    Delete target dir.

--- target/classes/db/migration/V1__Create.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tsubuyaki (
-  id bigserial PRIMARY KEY,
-  txt varchar(100) NOT NULL,
-  version bigint NOT NULL,
-  update_time timestamp NOT NULL DEFAULT current_timestamp,
-  create_time timestamp NOT NULL DEFAULT current_timestamp
-);
