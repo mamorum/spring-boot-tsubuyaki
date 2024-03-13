ebfd3156da6dab8552e1d765d7db55af1e994ff7
mamorum <mamorum@users.noreply.github.com>
2024-03-13 20:58:53 +0900

    Rename static files.

--- src/main/resources/static/js/tweet.js
+++ src/main/resources/static/js/tweet.js
@@ -1,32 +1,18 @@
 $(function() {
-
   // utilities.
   function render(data) {
-    var tmpl = $('#tweet-tmpl').html();
+    let tmpl = $('#tweet-tmpl').html();
     Mustache.parse(tmpl);
-    var rendered = Mustache.render(tmpl, data);
+    let rendered = Mustache.render(tmpl, data);
     $('#tweet-list').prepend(rendered);
   }
-  function format(msecString) {
-    var d = new Date(Number(msecString));
-    if (d.toLocaleDateString() === (new Date()).toLocaleDateString()) {
-      // today. return 'hh:mm'.
-      return ('0' + d.getHours()).slice(-2) + ':' +
-            ('0' + d.getMinutes()).slice(-2);
-    }
-    // not today. return 'yyyy.mm.dd'.
-    return d.getFullYear() + '.' +
-          ('0' + (d.getMonth()+1)).slice(-2) + '.' +
-          ('0' + d.getDate()).slice(-2);
+  function deleteSec(dateString) {
+    return dateString.substring(0, 16);
   }
-
 
   // onload.
   (function() {
-
     $('#txt').focus();
-
-    // read.
     $.ajax({
       url: '/tweet',
       method: 'get',
@@ -34,18 +20,15 @@ $(function() {
     }).then(function(data, status, jqxhr) {
       render(data);
       $('.date').each(function(index, e) {
-        $(e).html(format($(e).html()));
+        $(e).html(deleteSec($(e).html()));
       })
     });
   })();
-
   
   // create.
   $('#create').click(function() {
-
-    var txt = $('#txt').val();
+    let txt = $('#txt').val();
     if (txt === '') return;
-
     $.ajax({
       url: '/tweet',
       data: JSON.stringify({'txt':txt}),
@@ -54,24 +37,22 @@ $(function() {
       cache: false
     }).then(function(data, status, jqxhr) {
       render(data);
-      var $date = $('.tweet:first').find('.date');
-      $date.html(format($date.html()));
+      let $date = $('.tweet:first').find('.date');
+      $date.html(deleteSec($date.html()));
       $('#txt').val('').focus();
     });
   });
 
   // update.
-  var $tweet;
+  let $tweet;
   $('body').on('click', '.edit', function() {
     $tweet = $(this).closest('.tweet');
     $('#new-txt').val($tweet.find('.txt p').html());
     $('#modal').modal();
   });
   $('#modal-update').click(function() {
-
-    var txt = $('#new-txt').val();
-    var url = '/tweet/' + $tweet.data('id');
-
+    let txt = $('#new-txt').val();
+    let url = '/tweet/' + $tweet.data('id');
     $.ajax({
       url: url,
       data: {'txt':txt},
@@ -85,12 +66,9 @@ $(function() {
 
   // delete.
   $('body').on('click', '.delete', function() {
-
     if (!confirm("削除しますか？")) return;
-
-    var $tweet = $(this).closest('.tweet');
-    var url = '/tweet/' + $tweet.data('id');
-
+    let $tweet = $(this).closest('.tweet');
+    let url = '/tweet/' + $tweet.data('id');
     $.ajax({
       url: url,
       method: 'delete',
